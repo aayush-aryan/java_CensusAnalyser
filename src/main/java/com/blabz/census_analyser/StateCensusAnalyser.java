34f660d83f0342b814a1181c59971c1fb8f5641a
aayush-aryan <log2aayusharyan@gmail.com>
2020-04-02 13:55:50 +0530

    [Aayush]added. welcome message for census_analyser

---
+++ src/main/java/com/blabz/census_analyser/StateCensusAnalyser.java
@@ -0,0 +1,7 @@
+package com.blabz.census_analyser;
+
+public class StateCensusAnalyser {
+    public static void main(String[] args){
+        System.out.println("Welcome to CensusAnalyser");
+    }
+}
