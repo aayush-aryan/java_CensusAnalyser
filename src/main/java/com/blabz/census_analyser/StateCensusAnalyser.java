036338dbe5bb14d9e248c93d0ec17557259f2160
aayush-aryan <log2aayusharyan@gmail.com>
2020-04-07 21:01:08 +0530

    [Aayush]added. taking the list of census data Instead of iterating through file

--- src/main/java/com/blabz/census_analyser/StateCensusAnalyser.java
+++ src/main/java/com/blabz/census_analyser/StateCensusAnalyser.java
@@ -5,36 +5,37 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.List;
 import java.util.stream.StreamSupport;
 
 public class StateCensusAnalyser {
-        public int loadIndiaCensusData(String csvFilePath) throws StateCensusAnalyserException {
-            try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));){
-                ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-                Iterator<CSVStateCensus> censusCSVIterator = csvBuilder.getCSVFileIterator(reader,CSVStateCensus.class);
-                return getCount(censusCSVIterator);
-            }catch (IOException e){
-                throw new StateCensusAnalyserException(e.getMessage(),StateCensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
-            }catch (CSVBuilderException e) {
-                throw new StateCensusAnalyserException(e.getMessage(), e.type.name());
-            }
+    public int loadIndiaCensusData(String csvFilePath) throws StateCensusAnalyserException {
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));){
+            ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
+            List censusCSVIList = csvBuilder.getCSVFileList(reader,CSVStateCensus.class);
+            return censusCSVIList.size();
+        }catch (IOException e){
+            throw new StateCensusAnalyserException(e.getMessage(),StateCensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }catch (CSVBuilderException e) {
+            throw new StateCensusAnalyserException(e.getMessage(), e.type.name());
         }
-        public int loadIndiaStateCodeData(String csvFilePath) throws StateCensusAnalyserException {
-            try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))){
-                ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-                Iterator<CSVStates> stateCSVIterator = (Iterator<CSVStates>) csvBuilder.getCSVFileIterator(reader,CSVStates.class);
-                return getCount(stateCSVIterator);
-            }catch (IOException e) {
-                throw new StateCensusAnalyserException(e.getMessage(), StateCensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
-            }catch (CSVBuilderException e) {
-                throw new StateCensusAnalyserException(e.getMessage(), e.type.name());
-            }
+    }
+    public int loadIndiaStateCodeData(String csvFilePath) throws StateCensusAnalyserException {
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))){
+            ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
+            List stateCSVList = csvBuilder.getCSVFileList(reader,CSVStates.class);
+            return stateCSVList.size();
+        }catch (IOException e) {
+            throw new StateCensusAnalyserException(e.getMessage(), StateCensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }catch (CSVBuilderException e) {
+            throw new StateCensusAnalyserException(e.getMessage(), e.type.name());
         }
-        private <E> int getCount(Iterator<E> iterator){
-            Iterable<E> csvIterable = () -> iterator;
-            int numOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
-            return numOfEntries;
-        }
+    }
+    private <E> int getCount(Iterator<E> iterator){
+        Iterable<E> csvIterable = () -> iterator;
+        int numOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
+        return numOfEntries;
+    }
 }
 
 
--- src/main/java/com/blabz/census_analyser/StateCensusAnalyserException.java
+++ src/main/java/com/blabz/census_analyser/StateCensusAnalyserException.java
@@ -2,11 +2,19 @@ package com.blabz.census_analyser;
 
 public class StateCensusAnalyserException extends Exception{
     public enum ExceptionType{
-        CENSUS_FILE_PROBLEM,UNABLE_TO_PARSE
+        CENSUS_FILE_PROBLEM,UNABLE_TO_PARSE;
     }
     public ExceptionType type;
-    public StateCensusAnalyserException(String message, ExceptionType type){
+    public StateCensusAnalyserException(String message,ExceptionType type){
         super(message);
         this.type = type;
+    }
+    public StateCensusAnalyserException(String message,ExceptionType type,Throwable cause){
+        super(message,cause);
+        this.type = type;
+    }
+    public StateCensusAnalyserException(String message,String name){
+        super(message);
+        this.type = ExceptionType.valueOf(name);
     }
 }
